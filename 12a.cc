c681072c5446ab0101ddabad31f062d9bd5759a8
Frigusterio <jordiryhr@gmail.com>
2024-12-12 11:39:02 +0100

    day 12 immense spaghetti

---
+++ 12a.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <queue>
+#define x first
+#define y second
+using namespace std;
+
+const vector<pair<int, int>> DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+int main()
+{
+    ifstream file("12.test");
+    string s;
+
+    vector<string> gardenBad;
+
+    while (getline(file, s)) gardenBad.push_back(s);
+
+    int n = gardenBad.size();
+    int m = gardenBad[0].size();
+
+    vector<vector<int>> garden (n, vector<int> (m, -1));
+    int id = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            //cout << "Checking " << i << ", " << j << ": " << gardenBad[i][j] << endl;
+            if (garden[i][j] == -1)
+            {
+                //cout << i << ' ' << j << endl;
+                queue<pair<int, int>> region;
+                region.push({i, j});
+                while (!region.empty())
+                {
+                    pair<int, int> plot = region.front();
+                    region.pop();
+                    if (garden[plot.x][plot.y] != -1) continue;
+                    garden[plot.x][plot.y] = id;
+                    //cout << plot.x << ' ' << plot.y << ": " << gardenBad[plot.x][plot.y] << ", " << garden[plot.x][plot.y] << endl;
+                    for (int d = 0; d < 4; d++)
+                    {
+                        pair<int, int> nextPlot = {plot.x + DIRS[d].x, plot.y + DIRS[d].y};
+                        bool validPos = 0 <= nextPlot.x and nextPlot.x < n and 0 <= nextPlot.y and nextPlot.y < m;
+                        if (validPos and gardenBad[nextPlot.x][nextPlot.y] == gardenBad[plot.x][plot.y] and garden[nextPlot.x][nextPlot.y] == -1)
+                        {
+                            //cout << "Push " << nextPlot.x << ' ' << nextPlot.y << ": " << gardenBad[nextPlot.x][nextPlot.y] << ", " << garden[nextPlot.x][nextPlot.y] << endl;
+                            region.push(nextPlot);
+                        }
+                    }
+                }
+                id++;
+            }
+        }
+    }
+
+    vector<int> areas (id, 0), perimeters (id, 0);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            areas[garden[i][j]]++;
+            for (int d = 0; d < 4; d++)
+            {
+                int i2 = i + DIRS[d].x, j2 = j + DIRS[d].y;
+                bool validPos = 0 <= i2 and i2 < n and 0 <= j2 and j2 < m;
+                if (!validPos or garden[i2][j2] != garden[i][j])
+                    perimeters[garden[i][j]]++;
+            }
+        }
+    }
+    
+    int price = 0;
+    for (int i = 0; i < id; i++)
+    {
+        price += areas[i] * perimeters[i];
+    }
+    cout << price << endl;
+}
